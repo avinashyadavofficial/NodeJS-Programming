65822ebcacc5c823d951eb7d62e63758005024f8
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-08 11:43:54 +0530

    [Avinash Yadav] : Day1: Modify  myFirst program

--- Day1/myFirst.js
+++ Day1/myFirst.js
@@ -6,7 +6,7 @@ listener = function (request, response) {
  response.writeHead(200, {'Content-Type': 'text/html'});
 
  // Send the response body as "Hello World"
- response.end('<h2 style="text-align: center;">Hello World</h2>');
+ response.end('Hello World');
 };
 
 server = http.createServer(listener);
