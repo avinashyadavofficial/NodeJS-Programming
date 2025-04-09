7ff63ec66f333cf2e07fbcd1bab94f6df35bfda2
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-09 17:39:46 +0530

    [Avinash Yadav]: Day2: Add method overriding

---
+++ Day2/OOP/methodOverriding.js
@@ -0,0 +1,59 @@
+// When a child object defines a method that has the same name as a method in its prototype (parent) —
+// the child’s version is used.
+const parent={
+    greet(){
+        console.log("Parent");
+    }
+}
+const child =Object.create(parent);
+child.greet=function(){
+    console.log("Child");
+}
+child.greet(); //Child
+
+//without overridden
+const dog = {
+    speak() {
+      console.log("dog sound");
+    }
+  };
+  
+  const a= Object.create(dog);
+  
+  a.speak(); // dog sound
+
+
+//to call parent even after overridden
+const c={
+    x(){
+        console.log("Parent");
+    }
+}
+const d=Object.create(c);
+d.x=function(){
+    console.log("Child");
+    c.x.call(this);
+}
+d.x(); //Parent
+
+
+//example
+class Fruit{
+    static print(){
+        console.log("Fruit");
+    }
+}
+class Apple extends Fruit {
+    print(){
+        console.log("Apple");
+        Fruit.print.call(this); // instead of writing this and static in parent class 
+                                // we can use super(print);
+
+    }
+}
+const ap=new Apple();
+ap.print();
+
+
+
+  
