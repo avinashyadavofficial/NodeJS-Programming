1255aba38b4dcd518619a0e3f60bd7983cbb1395
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-09 11:32:11 +0530

    [Avinash Yadav]: Day2: Add Polyfills and Transpillers

---
+++ Day2/polyfillAndTranspiller.js
@@ -0,0 +1,33 @@
+// A polyfill is a piece of code (usually JS) that adds support for a feature that doesn't 
+// exist in an older browser or environment.
+
+// Old browsers don't support Array.prototype.includes
+if (!Array.prototype.includes) {
+    Array.prototype.includes = function(value) {
+      return this.indexOf(value) !== -1;
+    };
+}
+console.log([1,2,3].includes(2));
+
+//Now even in old browsers that don't have includes(), the code still works!
+
+//Transpiller
+//A transpiler (like Babel) converts modern JavaScript (ES6+) into older syntax (ES5)
+//  that all browsers understand.
+
+//ex:
+//modern js
+const sum = (a, b) => a + b;
+
+//after transpile
+var sum1 = function(a, b) {
+  return a + b;
+};
+
+// /Transpilers make your code backward-compatible.
+
+/*
+Concept	What it does
+Polyfill	Adds missing features to older environments
+Transpiler	Rewrites new syntax into old-school JS
+*/
