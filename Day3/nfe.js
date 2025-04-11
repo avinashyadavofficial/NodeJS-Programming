d63a317d1f13d233fabb6281fcf5c824d9feee2f
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-11 15:33:52 +0530

    [Avinash Yadav]: Day3: Add nfe

---
+++ Day3/nfe.js
@@ -0,0 +1,27 @@
+//Named Function Expression
+//A Named Function Expression (NFE) is a function expression where the function has a name, 
+// even though it's assigned to a variable.
+
+const sayHi = function greet() {
+    console.log("Hello!");
+  };
+sayHi();
+
+let factorial=function fact(n){
+    if(n==1){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+console.log(factorial(5));
+
+const greet = function sayHello() {
+    console.log(typeof sayHello); //function
+  };
+  
+  greet();           
+  console.log(typeof sayHello); // undefined
+  console.log(typeof greet);//function
+  
+  
+  
