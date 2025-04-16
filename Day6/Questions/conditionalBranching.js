bfbf8aba37c86ec4aca21e4fe29ccef416f15b3d
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-16 15:09:22 +0530

    [Avinash Yadav]:Day6: Add conditional branching examples

---
+++ Day6/Questions/conditionalBranching.js
@@ -0,0 +1,13 @@
+// Write a function that takes a number and logs whether it's positive, negative, or zero using if-else.
+function sol(n){
+    if(n<0){
+        console.log("Negative");
+    }
+    else if(n==0){
+        console.log("zero");
+    }
+    else{
+        console.log("Positive");
+    }
+}
+sol(10);//Positive
