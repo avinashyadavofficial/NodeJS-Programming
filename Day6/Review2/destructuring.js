ce8cc969257fcd4905d711dfb9e623d13a269090
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-16 14:50:16 +0530

    [Avinash Yadav]:Day6: Add Can we change type and val of object using destructuring

---
+++ Day6/Review2/destructuring.js
@@ -0,0 +1,16 @@
+const a={
+    
+    age:25
+}
+// let {name,age}=a;
+// console.log(name);
+// console.log(age);
+// let {name:nam,age}=a;
+// console.log(nam);
+// console.log(age);
+let {name="Rahul",age}=a;
+console.log(name);
+console.log(age);
+
+
+
