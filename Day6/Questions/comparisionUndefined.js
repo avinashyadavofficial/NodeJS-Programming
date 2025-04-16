7d19141617bb4d0cc6c1f0e3d7182360caf70015
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-16 15:08:33 +0530

    [Avinash Yadav]:Day6: Add comparision of  undefined

---
+++ Day6/Questions/comparisionUndefined.js
@@ -0,0 +1,5 @@
+console.log(undefined==undefined); //true
+console.log(undefined===undefined);//true
+console.log(undefined==0);//false
+console.log(undefined<0);//false
+console.log(undefined>0);//false
