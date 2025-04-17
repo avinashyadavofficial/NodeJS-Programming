e8517e3a73f7657ac03bc7ca586611324ace8727
Avinash Yadav <ar5840@srmist.edu.in>
2025-04-17 17:46:48 +0530

    [Avinash Yadav]:Day7: Add Using flatmap to double each element of multi dimensional array

---
+++ Day7/flatmap.js
@@ -0,0 +1,31 @@
+// function flattenDeep(arr) {
+//     return arr.flatMap(item =>
+//       Array.isArray(item) ? flattenDeep(item) : [item]
+//     );
+//   }
+  
+//   const arrayND = [ [ [1, 2], [3, [4]] ], [ [5], [6, [7, 8]] ] ];
+  
+//   const flat = flattenDeep(arrayND);
+//   console.log(flat); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+function flatMapDeep(arr,fun){
+    // let ans=arr.flatMap(ele=>
+    //     Array.isArray(ele)?flatMapDeep(ele,fun):[fun(ele)]
+    // )
+    // return ans;
+
+    //u can also write like this
+    return arr.flatMap(ele=>
+        Array.isArray(ele)?flatMapDeep(ele,fun):[fun(ele)]
+    )
+}
+// function double(x){
+//     return x*2;
+// }
+const arr=[ [ [1, 2], [3, [4]] ], [ [5], [6, [7, 8]] ] ];
+// console.log(flatMapDeep(arr,double));
+// in place of defining double a function we can directly assign while printing
+console.log(flatMapDeep(arr,a=>a*2));
+
+  
